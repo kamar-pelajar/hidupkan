b854c1804656aeb1d660bf1521821eb03a0e0e23
Xave Senduk <fransiscusxave@gmail.com>
2024-06-17 14:16:40 +0800

    feat: init user migration & seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,14 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'nama_lengkap' => fake()->name(),
+            'asal' => fake()->city(),
+            'domisili' => fake()->city(),
+            'no_ktp' => fake()->unique()->numerify('################'),
             'email' => fake()->unique()->safeEmail(),
+            'no_telp' => fake()->unique()->numerify('##########'),
+            'no_telp_whatsapp' => fake()->unique()->numerify('##########'),
+            'tanggal_lahir' => fake()->date(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
